5872292c0115b6a6b128ac0451f10dc714107729
Eliot <saltycrane@gmail.com>
2022-07-07 15:42:01 -0700

    verify password
    https://www.passportjs.org/tutorials/password/verify/

--- app.js
+++ app.js
@@ -1,10 +1,50 @@
+const crypto = require("crypto");
 const express = require("express");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const db = require("./db");
 
 const PORT = 3000;
+
+// configure passport local authentication
+passport.use(
+  new LocalStrategy((username, password, cb) => {
+    db.get("SELECT * FROM users WHERE username = ?", [username], (err, row) => {
+      if (err) {
+        return cb(err);
+      }
+      if (!row) {
+        return cb(null, false, {
+          message: "Incorrect username or password.",
+        });
+      }
+      crypto.pbkdf2(
+        password,
+        row.salt,
+        310000,
+        32,
+        "sha256",
+        (err, hashedPassword) => {
+          if (err) {
+            return cb(err);
+          }
+          if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
+            return cb(null, false, {
+              message: "Incorrect username or password.",
+            });
+          }
+          return cb(null, row);
+        }
+      );
+    });
+  })
+);
 
 const app = express();
 
 app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -13,6 +53,14 @@ app.get("/", (req, res) => {
 app.get("/login", (req, res) => {
   res.render("login");
 });
+
+app.post(
+  "/login/password",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
