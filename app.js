beabf6f382f9dbf223243e13c72147018d3a7da9
Eliot <saltycrane@gmail.com>
2022-07-07 15:09:22 -0700

    add ejs view templates

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require("express");
+
+const PORT = 3000;
+
 const app = express();
-const port = 3000;
+
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("index");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
